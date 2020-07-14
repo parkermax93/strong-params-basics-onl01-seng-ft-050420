8eb4b2bd3891e0fb71102b1567617f5122ce2bd7
Parker Williamson <parker.williamson@gmail.com>
2020-07-14 19:14:18 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,25 +11,26 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
-  def create
-    @post = Post.new(post_params(:title, :description))
-    @post.save
-    redirect_to post_path(@post)
-  end
-   
-  def update
-    @post = Post.find(params[:id])
-    @post.update(post_params(:title))
-    redirect_to post_path(@post)
-  end
-   
-  private
+	def create
+		@post = Post.new(params.require(:post).permit(:title, :description))
+		@post.save
+		redirect_to post_path(@post)
+	end
 
-  def post_params(*args)
-    params.require(:post).permit(*args)
-  end
+	def update
+		@post = Post.find(params[:id])
+		@post.update(params.require(:post).permit(:title))
+		redirect_to post_path(@post)
+	end
 
 	def edit
 	  @post = Post.find(params[:id])
 	end
+
+	private
+ 
+		def post_params
+			params.require(:post).permit(:title, :description)
+		end
+		
 end
